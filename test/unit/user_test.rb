baa58dd826e8907e71b33400fd1465864d6963f6
Sander <sander@ubuntu.ubuntu-domain>
2012-02-01 12:28:48 +0100

    Merge

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,35 @@
-require 'test_helper'
+# require 'carrierwave/test/matchers'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+describe ImageUploader do
+  include CarrierWave::Test::Matchers
+
+  before do
+    ImageUploader.enable_processing = true
+    @uploader = ImageUploader.new(@user, :picture)
+    @uploader.store!(File.open("~/Afbeeldingen/Art_Nature.jpg"))
+  end
+
+  after do
+    ImageUploader.enable_processing = false
+    @uploader.remove!
+  end
+
+  context 'the thumb version' do
+    it "should scale down a landscape image to be exactly 64 by 64 pixels" do
+      @uploader.thumb.should have_dimensions(64, 64)
+    end
+  end
+
+  context 'the small version' do
+    it "should scale down a landscape image to fit within 200 by 200 pixels" do
+      @uploader.small.should be_no_larger_than(200, 200)
+    end
+  end
+
+  it "should make the image readable only to the owner and not executable" do
+    @uploader.should have_permissions(0600)
+  end
 end
+	end
